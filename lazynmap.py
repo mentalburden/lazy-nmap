4364f85f778a285930bed61fe5ab6f66eecfcae6
Nahta Realnaame <47120653+mentalburden@users.noreply.github.com>
2020-02-20 23:05:56 -0800

    Update lazynmap.py
    Needed cidr notation handling from iplist.

--- lazynmap.py
+++ lazynmap.py
@@ -2,52 +2,87 @@
 
 import asyncio as asyncio
 
-filepath = 'iplist.txt'
-cmdToRun = 'nmap -F --open ' #needs a space at the end
+iplistpath = 'iplist.txt'
+outfilepath = '' #nothing or absolute path
+liveHostsCmd = "nmap -sn 45.55.24.10/24 -oG - | awk '{ print $2 }'"
+cmdToRun = 'nmap -T4 --min-parallelism 1 --max-parallelism 1 -F --open ' #needs a space at the end
 iptargets = []
-tasks = []
+livetargets = []
+livehosttasks = []
+portscantasks = []
 
-async def run(ip):
-    cmd = cmdToRun + str(ip)
-    outfilename = ip.rstrip("\n")+'-nmap-scan.txt'
+
+async def runlivehosts(ip):
+    cmd = "nmap -sn " + str(ip.rstrip("\n")) + " -oG - | awk '/Up$/{print $2}'"
+    proc = await asyncio.create_subprocess_shell(cmd,stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+    stdout, stderr = await proc.communicate()
+    if stdout:
+        chonk = stdout.decode()
+        for line in chonk.splitlines():
+            livetargets.append(line)
+    if stderr:
+        print("very broken")
+    await asyncio.sleep(0.3)
+
+
+async def runportscan(ip):
+    cmd = cmdToRun + str(ip.rstrip("\n"))
+    outfilename = outfilepath+ip.rstrip("\n")+'-nmap-scan.txt'
     print(outfilename)
     nmapoutfile = open(outfilename, "a+")
     proc = await asyncio.create_subprocess_shell(cmd,stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
     stdout, stderr = await proc.communicate()
-    print(f'[{ip!r} exited with {proc.returncode}]')
+#    print(f'[{ip!r} exited with {proc.returncode}]')
     if stdout:
         nmapoutfile.write(stdout.decode())
-        print(f'{stdout.decode()}')
+#        print(f'{stdout.decode()}')
     if stderr:
         nmapoutfile.write(stderr.decode())
-        print(f'{stderr.decode()}')
-    await asyncio.sleep(0.3)
+#        print(f'{stderr.decode()}')
+    await asyncio.sleep(10)
     nmapoutfile.close()
 
-async def loopmanager():
+
+async def livescanloopmanager():
+    #start livehost scan to clean up actual target list and take care of cidrs
     for i in range(len(iptargets)):
         try:
-            tasks.append(i)
-            tasks[i] = loop.create_task(run(iptargets[i]))
+            livehosttasks.append(i)
+            livehosttasks[i] = loop.create_task(runlivehosts(iptargets[i]))
         except IndexError:
             pass
     await asyncio.sleep(0.5)
-    for i in range(len(tasks)):
-        await tasks[i]
+    for i in range(len(livehosttasks)):
+        await livehosttasks[i]
 
-def readInFile(filepath):
-    with open(filepath) as fp:
+
+async def portscanloopmanager():
+    #start portscans for each livehost
+    for i in range(len(livetargets)):
+        try:
+            portscantasks.append(i)
+            portscantasks[i] = loop.create_task(runportscan(livetargets[i]))
+        except IndexError:
+            pass
+    await asyncio.sleep(0.5)
+    for i in range(len(portscantasks)):
+        await portscantasks[i]
+
+
+def readInFile(iplistpath):
+    with open(iplistpath) as fp:
         line = fp.readline()
         jobnumber = 1
         while line:
-                iptargets.append(line)
+                iptargets.append(line.rstrip("\n"))
                 #print("debug: - {}".format(line.strip()))
                 line = fp.readline()
                 jobnumber += 1
 
+
 #main starts here
-print("starting \'" + cmdToRun + "\' jobs for:")
 if __name__ == '__main__':
-    readInFile(filepath)
+    readInFile(iplistpath)
     loop = asyncio.get_event_loop()
-    chonker = loop.run_until_complete(loopmanager())
+    chonker = loop.run_until_complete(livescanloopmanager())
+    chonker2 = loop.run_until_complete(portscanloopmanager())
